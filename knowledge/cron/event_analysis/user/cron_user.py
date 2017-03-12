8637666ae98b05903a7b0ad79f3b451475e3a580
linhaobuaa <1257819385@qq.com>
2017-03-12 16:28:21 +0800

    jln

--- knowledge/cron/event_analysis/user/cron_user.py
+++ knowledge/cron/event_analysis/user/cron_user.py
@@ -10,6 +10,8 @@ from global_config import event_analysis_name,event_type,event_text,event_text_t
 from global_utils import es_event,es_user_portrait,portrait_index_name,portrait_index_type
 from global_utils import bci_day_pre,bci_day_type
 from parameter import RUN_TYPE,RUN_TEST_TIME
+from global_utils import es_user_profile,profile_index_name,profile_index_type
+from global_config import user_type,auth_type,user_list,auth_list
 
 def get_users(topic,begin_ts,end_ts):
 	uid_list = set()
@@ -51,11 +53,24 @@ def get_users(topic,begin_ts,end_ts):
 
 	user = sorted(user_influence_dict.iteritems(),key=lambda x:x[1],reverse=True)[:100]
 	#print user
+	user_dict = {}
+	for i in user:
+		try:
+			result = es_user_profile.get(index=profile_index_name,doc_type=profile_index_type,id=i[0])
+			u_type = result['_source']['verified_type']
+			if u_type in auth_list:
+				u_type = auth_type
+			else:
+				u_type = user_type
+			user_dict[i[0]] = {'user_type':u_type,'influ':i[1]}
+		except:
+			user_dict[i[0]] = {'user_type':user_type,'influ':i[1]}
+
 
 	try:
-		es_event.update(index=event_analysis_name,doc_type=event_type,id=topic,body={'doc':{'user_results':json.dumps(user)}})
+		es_event.update(index=event_analysis_name,doc_type=event_type,id=topic,body={'doc':{'user_results':json.dumps(user_dict)}})
 	except Exception,e:
-	    es_event.index(index=event_analysis_name,doc_type=event_type,id=topic,body={'user_results':json.dumps(user)})
+	    es_event.index(index=event_analysis_name,doc_type=event_type,id=topic,body={'user_results':json.dumps(user_dict)})
 
 
 if __name__ == '__main__':
