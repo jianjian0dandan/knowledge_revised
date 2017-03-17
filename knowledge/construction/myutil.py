b633c8bf57d24ed3d0faedd834cfe7fb362629f9
linhaobuaa <1257819385@qq.com>
2017-03-17 16:44:30 +0800

    ys

--- knowledge/construction/myutil.py
+++ knowledge/construction/myutil.py
@@ -2,7 +2,10 @@
 def get_type_key(item):
     if item == 1:
         return "uid"
-    elif item==2:
-        return "event"
+    elif item == 2:
+        return "event_id"
+    elif item == 0:
+        return 'org_id'
     else:
-        return ''
+        return 'uid'
+    
--- knowledge/construction/neo4j_event.py
+++ knowledge/construction/neo4j_event.py
@@ -77,12 +77,10 @@ def select_rels(node_key, node_id, node_name_index):
     Index = ManualIndexManager(graph)
     node_name = Index.get_index(Node, node_name_index)
     if not node_name:
-        print "node_index does not exist "
-        return None
+        return "Node_index does not exist "
     node = node_name.get(node_key, node_id)[0]
     if not node:
-        print "node does not exist"
-        return None
+        return "Node does not exist"
     c_string = "START start_node=node:%s(%s='%s') MATCH (start_node)-[r]-() return r" % (
         node_name_index, node_key, node_id)
     print c_string
@@ -93,38 +91,43 @@ def select_rels(node_key, node_id, node_name_index):
 
 
 # 对数据进行存放
-def create_person(node_type, node_key, node_id, node_name_index, attribute_dict=dict()):
+def create_person(node_type, node_key, node_id, node_name_index):
+    
     Index = ManualIndexManager(graph)
     node_name = Index.get_or_create_index(Node, node_name_index)
-    print node_name
     if node_name:
         exist = node_name.get(node_key, node_id)
-        print exist
         if exist:
-            person_node = exist[0]
-            for k, v in attribute_dict.iteritems():
-                person_node[k] = v
-            graph.push(person_node)
+            return 'Node Exist'#节点已存在
         else:
-            person_node = Node(node_type, uid=node_id)
-            for k, v in attribute_dict.iteritems():
-                person_node[k] = v
+            if node_key == "uid":
+                person_node = Node(node_type, uid=node_id)
+            elif node_key == "org_id":
+                person_node = Node(node_type, org_id=node_id)
+            elif node_key == "event_id":
+                person_node = Node(node_type, event_id=node_id)
+            elif node_key == "event":
+                person_node = Node(node_type, event=node_id)
+            elif node_key == "group":
+                person_node = Node(node_type, group=node_id)
+            else:
+                return 'Node Type Wrong'#节点类型错误
             graph.create(person_node)
             node_name.add(node_key, node_id, person_node)
-            print "create person success"
-            return True
+            return 'Node Success'#创建节点成功
+    else:
+        return 'Node Wrong'#创建节点失败
 
 
 # 对多个节点和类建立关系
-def create_rel_from_uid2group(node_key, uid_list, node_index_name, group_rel, group_key, group_id, group_index_name):
+def create_rel_from_uid2group(node_key, uid_list, node_name, group_rel, group_key, group_id, group_name):
     count = 0
     Index = ManualIndexManager(graph)
-    node_index = Index.get_index(Node, node_index_name)
-    group_index = Index.get_index(Node, group_index_name)
+    node_index = Index.get_index(Node, node_name)
+    group_index = Index.get_index(Node, group_name)
     tx = graph.begin()
     if not (node_index and group_index):
-        print "node or group does not exist"
-        return None
+        return "Node or group index does not exist"
     for uid in uid_list:
         node = node_index.get(node_key, uid)[0]
         group_node = group_index.get(group_key, group_id)[0]
@@ -137,6 +140,8 @@ def create_rel_from_uid2group(node_key, uid_list, node_index_name, group_rel, gr
             tx = graph.begin()
     tx.commit()
 
+    return 'Success'
+
 
 # 多对多创建节点关系([node1_type,node1_id],rel,[node2_type,node2_id])
 def nodes_rels(list):
@@ -145,10 +150,11 @@ def nodes_rels(list):
     Index = ManualIndexManager(graph)
     node_index = Index.get_index(Node, node_index_name)
     event_index = Index.get_index(Node, event_index_name)
+    org_index = Index.get_index(Node, org_index_name)
     tx = graph.begin()
-    if not (node_index and event_index):
-        print "node or group does not exist"
-        return "0"
+    if not (node_index and event_index and org_index):
+        return 'Relation Wrong'
+    
     for item in list:
         node1_key = get_type_key(item[0][0])
         node2_key = get_type_key(item[2][0])
@@ -159,62 +165,132 @@ def nodes_rels(list):
             continue
         flag = 0
         if node1_key == "uid":
-            if node2_key == 'uid':
+            if node2_key == 'uid':#'Person-Person'
                 node1 = node_index.get(node1_key, node1_uid)
                 if (node1 == []):
-                    result =result + ",%d" % node1_uid
+                    result =result + ",%s" % node1_uid
                     flag=1
                 node1 = node1[0]
                 node2 = node_index.get(node2_key, node2_uid)
                 if (node2 == []):
-                    result = result +",%d" % node2_uid
+                    result = result +",%s" % node2_uid
                     flag=1
                 node2 = node2[0]
-                print 11
 
-            else:
+            elif node2_key == 'event_id':#'Person-Event'
                 node1 = node_index.get(node1_key, node1_uid)
                 if (node1 == []):
-                    result =result + ",%d" % node1_uid
+                    result =result + ",%s" % node1_uid
                     flag=1
                 else:
                     node1 = node1[0]
                 node2 = event_index.get(node2_key, node2_uid)
                 if (node1 == []):
-                    result = result +",%d" % node2_uid
+                    result = result +",%s" % node2_uid
                     flag=1
                 else:
                     node2 = node2[0]
-                print 12
-        else:
+
+            else:#'Person-Org'
+                node1 = node_index.get(node1_key, node1_uid)
+                if (node1 == []):
+                    result =result + ",%s" % node1_uid
+                    flag=1
+                else:
+                    node1 = node1[0]
+                node2 = org_index.get(node2_key, node2_uid)
+                if (node1 == []):
+                    result = result +",%s" % node2_uid
+                    flag=1
+                else:
+                    node2 = node2[0]
+
+        elif node1_key == 'event_id':#'Event-Person'
             if node2_key == 'uid':
                 node1 = event_index.get(node1_key, node1_uid)
                 if (node1 == []):
-                    result =result + ",%d" % node1_uid
+                    result =result + ",%s" % node1_uid
                     flag=1
                 else:
                     node1 = node1[0]
                 node2 = node_index.get(node2_key, node2_uid)
                 if (node1 == []):
-                    result = result +",%d" % node2_uid
+                    result = result +",%s" % node2_uid
                     flag=1
                 else:
                     node2 = node2[0]
-                print 21
-            else:
+
+            elif node2_key == 'event_id':#'Event-Event'
                 node1 = event_index.get(node1_key, node1_uid)
                 if (node1 == []):
-                    result =result + ",%d" % node1_uid
+                    result =result + ",%s" % node1_uid
                     flag=1
                 else:
                     node1 = node1[0]
                 node2 = event_index.get(node2_key, node2_uid)
                 if (node1 == []):
-                    result = result +",%d" % node2_uid
+                    result = result +",%s" % node2_uid
                     flag=1
                 else:
                     node2 = node2[0]
-                print 22
+
+            else:#'Event-Org'
+                node1 = event_index.get(node1_key, node1_uid)
+                if (node1 == []):
+                    result =result + ",%s" % node1_uid
+                    flag=1
+                else:
+                    node1 = node1[0]
+                node2 = org_index.get(node2_key, node2_uid)
+                if (node1 == []):
+                    result = result +",%s" % node2_uid
+                    flag=1
+                else:
+                    node2 = node2[0]
+
+        else:
+            if node2_key == 'uid':#'Org-Person'
+                node1 = org_index.get(node1_key, node1_uid)
+                if (node1 == []):
+                    result =result + ",%s" % node1_uid
+                    flag=1
+                else:
+                    node1 = node1[0]
+                node2 = node_index.get(node2_key, node2_uid)
+                if (node1 == []):
+                    result = result +",%s" % node2_uid
+                    flag=1
+                else:
+                    node2 = node2[0]
+
+            elif node2_key == 'event_id':#'Org-Event'
+                node1 = org_index.get(node1_key, node1_uid)
+                if (node1 == []):
+                    result =result + ",%s" % node1_uid
+                    flag=1
+                else:
+                    node1 = node1[0]
+                node2 = event_index.get(node2_key, node2_uid)
+                if (node1 == []):
+                    result = result +",%s" % node2_uid
+                    flag=1
+                else:
+                    node2 = node2[0]
+
+            else:#'Org-Org'
+                node1 = org_index.get(node1_key, node1_uid)
+                if (node1 == []):
+                    result =result + ",%s" % node1_uid
+                    flag=1
+                else:
+                    node1 = node1[0]
+                node2 = org_index.get(node2_key, node2_uid)
+                if (node1 == []):
+                    result = result +",%s" % node2_uid
+                    flag=1
+                else:
+                    node2 = node2[0]
+
         if flag == 0:
             rel = Relationship(node1, rel, node2)
             tx.create(rel)
@@ -223,7 +299,7 @@ def nodes_rels(list):
             tx.commit()
             tx = graph.begin()
     tx.commit()
-    return 'success'
+    return 'Relation Success'
 
 
 
@@ -232,67 +308,63 @@ def create_node_or_node_rel(node_key1, node1_id, node1_index_name, rel, node_key
     Index = ManualIndexManager(graph)
     node_index = Index.get_index(Node, node1_index_name)
     group_index = Index.get_index(Node, node2_index_name)
-    print node_index
-    print group_index
+
     tx = graph.begin()
     if len(node_index.get(node_key1, node1_id)) == 0:
-        print "node1 does not exist"
-        return 'node1 does not exist'
-    print node2_id, '======='
-    print group_index.get(node_key2, node2_id)
-    print 'node_key2', node_key2
+        return 'Node1 does not exist'
+
     if len(group_index.get(node_key2, node2_id)) == 0:
-        print "node2 does not exist"
-        return "node2 does not exist"
+        return "Node2 does not exist"
+
     node1 = node_index.get(node_key1, node1_id)[0]
     node2 = group_index.get(node_key2, node2_id)[0]
     if not (node1 and node2):
-        print "node does not exist"
-        return None
+        return "Node1 and Node2 do not exist"
+
     c_string = "START start_node=node:%s(%s='%s'),end_node=node:%s(%s='%s') MATCH (start_node)-[r:%s]->(end_node) RETURN r" \
                % (node1_index_name, node_key1, node1_id, node2_index_name, node_key2, node2_id, rel)
-    print c_string
+
     result = graph.run(c_string)
     # print result
     rel_list = []
     for item in result:
         rel_list.append(item)
-    print rel_list
+
     if not rel_list:
         rel = Relationship(node1, rel, node2)
         graph.create(rel)
-        print "create success"
+        return "Create success"
     else:
-        print "The current two nodes already have a relationship"
-    return True
+        return "The current two nodes already have a relationship"
 
 
 # 对节点进行更新
-def update_node(key_id, uid, node_index_name, attribute_dict):
+def update_node(key_id, uid, node_name, attribute_dict):
     Index = ManualIndexManager(graph)
-    node_index = Index.get_index(Node, node_index_name)
+    node_index = Index.get_index(Node, node_name)
     node = node_index.get(key_id, uid)[0]
-    print node
     if not node:
-        print "no such node"
-        sys.exit(0)
+        return "No such node"
 
     for k, v in attribute_dict.iteritems():
         node[k] = v
 
     graph.push(node)
-    print "update success"
-    return True
-
+    return "Update success"
 
 # 对节点间的rel进行更新
 def update_node_or_node_rel(node_key1, node1_id, node1_index_name, old_rel, new_rel, node_key2, node2_id,
                             node2_index_name):
     # 需要对节点的关系进行删除以后在进行添加。
-    delete_rel(node_key1, node1_id, node1_index_name, old_rel, node_key2, node2_id, node2_index_name)
-    create_node_or_node_rel(node_key1, node1_id, node1_index_name, old_rel, node_key2, node2_id, node2_index_name)
-    print "update rel success"
-    return True
+    rel_flag = delete_rel(node_key1, node1_id, node1_index_name, old_rel, node_key2, node2_id, node2_index_name)
+    if rel_flag == "Node_index does not exist" or rel_flag == "Node does not exist":
+        return 'Delete fail'
+        
+    node_flag = create_node_or_node_rel(node_key1, node1_id, node1_index_name, old_rel, node_key2, node2_id, node2_index_name)
+    if node_flag == "Node1 does not exist" or node_flag == "Node2 does not exist" or node_flag == "Node1 and Node2 do not exist":
+        return 'Recreate fail'
+
+    return "Update rel success"
 
 
 # 删除一个rel
@@ -302,46 +374,44 @@ def delete_rel(node_key1, node1_id, node1_index_name, rel, node_key2, node2_id, 
     node1_index = Index.get_index(Node, node1_index_name)
     node2_index = Index.get_index(Node, node2_index_name)
     if not (node1_index or node2_index):
-        print "node_index does not exist"
-        return None
+        return "Node_index does not exist"
     node1 = node1_index.get(node_key1, node1_id)[0]
     node2 = node2_index.get(node_key2, node2_id)[0]
     if not (node1 or node2):
-        print ("node does not exist")
+        return "Node does not exist"
     c_string = "START start_node=node:%s(%s='%s'),end_node=node:%s(%s='%s') MATCH (start_node)-[r:%s]-(end_node) RETURN r" % (
         node1_index_name, node_key1, node1_id, node2_index_name, node_key2, node2_id, rel)
     result = graph.run(c_string)
     for item in result:
         list.append(item)
     if not list:
-        print "Deleted rel does not exist"
-        return None
+        return "Deleted rel does not exist"
+
     c_string = "START start_node=node:%s(%s='%s'),end_node=node:%s(%s='%s') MATCH (start_node)-[r:%s]-(end_node) DELETE r" % (
         node1_index_name, node_key1, node1_id, node2_index_name, node_key2, node2_id, rel)
-    print c_string
+
     graph.run(c_string)
-    print "delete success"
-    return True
+    return "Relation delete success"
 
 
 def delete_node(node_key, node_id, node_index_name):
     Index = ManualIndexManager(graph)
     node_index = Index.get_index(Node, node_index_name)
     if not node_index:
-        print "node_index does not exist"
-        return None
+        return "Node_index does not exist"
+
     node = node_index.get(node_key, node_id)[0]
     if not node:
-        print "The node does not exist"
-        return None
+        return "Node does not exist"
+
     c_string = "START start_node=node:%s(%s='%s') MATCH (start_node)-[r]-()  DELETE r" % (
         node_index_name, node_key, node_id)
     graph.run(c_string)
-    print "delete rel success"
+
     c_string = "START start_node=node:%s(%s='%s') delete start_node" % (node_index_name, node_key, node_id)
     graph.run(c_string)
-    print "delete node success"
-    return True
+
+    return 'Node delete success'
 
 def select_event_es(result):
     print "111"
