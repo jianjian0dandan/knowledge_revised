7a31a2b4e62456eba42d3e43a6210646c478c5c2
linhaobuaa <1257819385@qq.com>
2017-03-29 14:50:01 +0800

    ys

--- knowledge/sysadmin/views.py
+++ knowledge/sysadmin/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from flask import Blueprint, url_for, render_template, request, abort, flash, session, redirect
+from flask import Blueprint, url_for, render_template, request, abort, flash, session, redirect, make_response, g
 from flask.ext.security import login_required
 import json
 import csv
@@ -8,6 +8,8 @@ import os
 import time
 from datetime import date
 from datetime import datetime
+from knowledge.global_utils import get_theme,get_group
+from knowledge.index.get_result import get_people,get_event,get_org
 
 mod = Blueprint('sysadmin', __name__, url_prefix='/sysadmin')
 
@@ -21,16 +23,28 @@ def myfocus():#导航页
 @login_required
 def myfocus_topic():#我的专题
 
-    return render_template('sysadmin/myfocus_topic.html')
+    submit_user = g.user.email
+    result = get_theme('',submit_user)
+    
+    return render_template('sysadmin/myfocus_topic.html',result = result)
 
 @mod.route('/group/')
 @login_required
 def myfocus_group():#我的群体
 
-    return render_template('sysadmin/myfocus_group.html')
+    submit_user = g.user.email
+    result = get_group('',submit_user)
+    
+    return render_template('sysadmin/myfocus_group.html',result = result)
 
 @mod.route('/focus/')
 @login_required
 def myfocus_focus():#我的关注
 
-    return render_template('sysadmin/myfocus_focus.html')
+    submit_user = g.user.email
+    people_result = get_people(submit_user,'all')
+    event_result = get_event(submit_user,'all')
+    org_result = get_org(submit_user,'all')
+    result = {'people':people_result,'event':event_result,'org':org_result}
+    
+    return render_template('sysadmin/myfocus_focus.html',result = result)
