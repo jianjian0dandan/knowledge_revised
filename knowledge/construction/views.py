361507907bef17b13e1959bc6e9f79be8e9804cf
linhaobuaa <1257819385@qq.com>
2017-04-20 08:59:00 +0800

    ymm

--- knowledge/construction/views.py
+++ knowledge/construction/views.py
@@ -53,9 +53,9 @@ def construction_graph_modify():#图谱编辑
 def add_node():
     return render_template('construction/addmap.html')
 
-@mod.route('/weiki/')
+@mod.route('/wiki/')
 def weiki():
-    return render_template('construction/weiki.html')
+    return render_template('construction/wiki.html')
 
 @mod.route('/show_in/')
 def ajax_recommentation_in():
